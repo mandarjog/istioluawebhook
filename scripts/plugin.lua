e88044c978c003a82312cc341d232149659d84ad
Mandar U Jog <mjog@google.com>
2018-03-28 14:21:19 -0700

    Add an example of basic auth

--- scripts/plugin.lua
+++ scripts/plugin.lua
@@ -1,6 +1,14 @@
+-- local base64 = require "base64"
+
 -- Called on the request path.
 function envoy_on_request(request_handle)
+  auth = request_handle:headers():get("authorization")
   request_handle:headers():add("x-lua-header", "true")
+  if auth == nil or auth == '' then
+    request_handle:respond(
+      {{[":status"] = "401", ["Content-Type"] = "text/plain", ["WWW-Authenticate"] = "Basic realm=luawebhook"}},
+           "authenticate-{nodeid}\nAny username is ok")
+  end
 end
 
 -- Called on the response path.
